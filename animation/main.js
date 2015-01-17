1e8f30ffc996719c688bea34e011610c4d0e94e7
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-17 22:51:30 +0000

    Adds codeacademy animation tutorial

---
+++ animation/main.js
@@ -0,0 +1,12 @@
+var myName = "Sanda Golcea";
+
+var red = [0, 100, 63];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var green = [120, 100, 40];
+var yellow = [50, 95, 57];
+
+// document.write(myName);
+letterColors = [red,green,yellow, blue,purple];
+drawName(myName,letterColors);
+bounceBubbles();
