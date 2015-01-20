c82ec4f392eae1e432fd3e4dec1b0c85fd4431be
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-20 12:05:20 +0000

    Finishes FizzBuzz

--- jasmineFizzBuzz/js/FizzBuzz.js
+++ jasmineFizzBuzz/js/FizzBuzz.js
@@ -9,3 +9,28 @@ FizzBuzz.prototype.isDivisibleByThree = function(number) {
 FizzBuzz.prototype.isDivisibleByFive = function(number) {
     return (number%5===0);
 };
+
+FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
+    if (this.isDivisibleByFive(number) && this.isDivisibleByThree(number)) return true;
+    return false;
+};
+
+FizzBuzz.prototype.play = function(number) {
+  if (this.isDivisibleByFifteen(number))
+    return "FizzBuzz";
+  else if (this.isDivisibleByThree(number))
+    return "Fizz";
+  else if (this.isDivisibleByFive(number))
+    return "Buzz";
+  else
+    return number;
+};
+
+// var fizzy = my_array.map( function(number) {return fizzbuzz.play(number)})
+// console play:
+// var array = [1,2,3,4,5,6,7,8,9,10];
+// fizzbuzz = new FizzBuzz();
+// var fizzy = array.map( function(number) {return fizzbuzz.play(number)} );
+// fizzy
+// TA-DA!
+// [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
--- jasmineFizzBuzz/spec/FizzBuzzSpec.js
+++ jasmineFizzBuzz/spec/FizzBuzzSpec.js
@@ -25,8 +25,41 @@ describe("FizzBuzz", function() {
         expect(fizzbuzz.isDivisibleByFive(11)).toBe(false);
       });
 
+      it("is divisible by 15", function() {
+        fizzbuzz = new FizzBuzz();
+        expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+      });
+
+
+      it("is NOT divisible by 15", function() {
+        fizzbuzz = new FizzBuzz();
+        expect(fizzbuzz.isDivisibleByFifteen(11)).toBe(false);
+      });
 
   });
 
+ describe("can", function() {
+
+      it("return FizzBuzz for number divisible by 15", function() {
+        fizzbuzz = new FizzBuzz();
+        expect(fizzbuzz.play(30)).toBe("FizzBuzz");
+      });
+
+      it("return Buzz for number divisible by 5", function() {
+        fizzbuzz = new FizzBuzz();
+        expect(fizzbuzz.play(10)).toBe("Buzz");
+      });
+
+      it("return Fizz for number divisible by 3", function() {
+        fizzbuzz = new FizzBuzz();
+        expect(fizzbuzz.play(3)).toBe("Fizz");
+      });
+
+      it("return number for number not divisible by 3 or 5", function() {
+        fizzbuzz = new FizzBuzz();
+        expect(fizzbuzz.play(31)).toBe(31);
+      });
+
+  });
 
 });
