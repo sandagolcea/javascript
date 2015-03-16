f75d5bebdabcce5393fcd94ffb02a00702799c34
matteomanzo <imatteo.manzo@gmail.com>
2015-03-16 19:48:29 +0000

    fixed html did not wrap inside html tags, now working

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var app = angular.module('bookstore', []);
+(function () {
+  var app = angular.module('bookstore', []);
+
+  app.controller('BookController', function() {
+    this.product = book
+
+  });
+
+  var book = {title: 'Pratical Object-Oriented Design in Ruby',
+              description: 'Learn OOP the right way! Sandi Metz explains how to best behave when we write a program',
+              author: 'Sandi Metz' }
+
+})();
